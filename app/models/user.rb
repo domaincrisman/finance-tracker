937ee6a903a8c4caa4f07fe4d9169a2fc0a0924b
Crisman <cmanzanore@gmail.com>
2016-02-15 19:47:30 +0100

    Add friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
-
+  has_many :friendship
+  has_many :friends, through: :friendship
+  
   def full_name
     return "#{first_name} #{last_name}".strip if (first_name || last_name)
     "Anonymous"
